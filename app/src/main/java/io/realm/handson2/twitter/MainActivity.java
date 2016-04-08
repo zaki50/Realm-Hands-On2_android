9fe3e3869bf5d476a6c0e8af147fa80c2518b7b6
Makoto Yamazaki <makoto1975@gmail.com>
2016-04-09 04:03:25 +0900

    「プロジェクトの作成」完了状態

---
+++ app/src/main/java/io/realm/handson2/twitter/MainActivity.java
@@ -0,0 +1,13 @@
+package io.realm.handson2.twitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
